4f37326305a9cd14c285f9d55abaf53c9b9d3f28
Balexo <oscar.vallejo@outlook.com>
2025-01-04 14:31:57 +0100

    Exercise31 solved

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,25 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise31 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)  {
+        String filepath = "src/main/resources/lorem.txt";
+        try (FileReader file = new FileReader(filepath);
+             BufferedReader buffer = new BufferedReader(file);
+        ) {
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
